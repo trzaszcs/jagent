bec5152da03535003d142546c6eb6bb525932ef5
Slawomir Trzaszczka <slawomir.trzaszczka@allegrogroup.com>
2016-08-24 12:57:07 +0200

    cleanup

--- sandbox/build-and-run.sh
+++ sandbox/build-and-run.sh
@@ -3,7 +3,9 @@ echo "1) BUILD jagent"
 ../gradlew build
 echo "2) COPY to sandbox"
 cp ../build/libs/jagent.jar .
-echo "3) RUN"
+echo "4) CLEANUP"
+rm -rf .gradle
+echo "5) RUN"
 echo "----------------------------------"
 java -javaagent:jagent.jar -jar dummy.jar
 echo "----------------------------------"
